c24f594e49aa533df77d99614cb13e30b8e0a23d
simorai <109289730+simorai@users.noreply.github.com>
2026-01-08 23:44:59 +0000

    Add Employer model and factory; refactor Job model and migrations to include employer relationship

--- example/app/Models/job.php
+++ example/app/Models/job.php
@@ -4,36 +4,10 @@ namespace App\Models;
 
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+class Job extends Model {
+    protected $table = 'job_listings';
 
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
