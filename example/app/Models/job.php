20b3d0cbbd8116fbc939013c783d96e1db2d7338
simorai <109289730+simorai@users.noreply.github.com>
2026-01-10 14:42:49 +0000

    Add Tag model, factory, and migration; implement many-to-many relationship with Job model

--- example/app/Models/job.php
+++ example/app/Models/job.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Job extends Model {
      use HasFactory;
@@ -16,5 +17,11 @@ class Job extends Model {
     public function employer()
     {
         return $this->belongsTo(Employer::class);
+    }
+
+        public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+
     }
 }
