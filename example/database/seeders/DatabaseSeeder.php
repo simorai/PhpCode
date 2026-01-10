7b7bedd58c4fe4205ff0718e153fff1e7724c577
simorai <109289730+simorai@users.noreply.github.com>
2026-01-10 16:34:37 +0000

    Add JobSeeder to seed job data; update DatabaseSeeder to include user details

--- example/database/seeders/DatabaseSeeder.php
+++ example/database/seeders/DatabaseSeeder.php
@@ -8,18 +8,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    // use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
